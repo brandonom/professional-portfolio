8224d0d874b7080c68669702786bab0b8d945016
brandonom <brandonomyers1@gmail.com>
2024-06-27 13:58:51 -0400

    new images

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import MainHeader from './components/Header'
 import Musik from './components/Musik'
 import Contact from './components/Contact'
 import Aboutme from './components/Aboutme'
-import UrbanVogue from './components/UrbanVogue';
+import UrbanVogue from './components/UrbanVogue'
+// import React from '../assets/React-icons.svg.png'
 
 
 function App() {
@@ -21,6 +22,10 @@ function App() {
       <h2>Brandon Myers</h2>
       
       <h3>I am webdesigner and developer whose passion is making striking and unique websites</h3>
+      <div>
+        <h1>My Skills</h1>
+      <img src=""></img>
+      </div>
       <h1 id="#Projects">My Work</h1>
       <Musik />
 
